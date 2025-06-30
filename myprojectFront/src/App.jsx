8cbdb3f71225f9225087a8b3e2ae774cba4ddd48
B Bhanuprasad <bhanuprasadbingi1999@gmail.com>
2025-06-30 14:47:11 +0530

    created pages

--- myprojectFront/src/App.jsx
+++ myprojectFront/src/App.jsx
@@ -1,12 +1,17 @@
- import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Components/Login";
+import Signup from "./Components/Signup";
  
- const App = () => {
-   return (
-     <div>
-       <h1>welocme to new project</h1>
-     </div>
-   )
- }
- 
- export default App
- 
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
